44e38a560a74f9a2b48a77e531af284bdfe56add
Vincent Mézino <vincent.mezino@gmail.com>
2016-01-16 22:08:31 +0100

    Add control on cat

--- game/cat.gd
+++ game/cat.gd
@@ -1,12 +1,32 @@
 
-extends Node
+extends RigidBody2D
 
-# member variables here, example:
-# var a=2
-# var b="textvar"
+var HORIZONTAL_LENGTH = 150
+var JUMP_LENGTH = -200
+
+var foot
 
 func _ready():
+	set_process(true)
+	
 	# Initialization here
-	pass
+	foot = get_node("Foot")
+	foot.add_exception(self)
+	
 
+func _process(delta):
+	# Get the controls
+	var move_left = Input.is_action_pressed("cat_left")
+	var move_right = Input.is_action_pressed("cat_right")
+	var jump = Input.is_action_pressed("cat_jump")
+
+	if foot.is_colliding():
+		if jump:		
+			set_axis_velocity(Vector2(0, JUMP_LENGTH))
+				
+		if move_left:
+			set_axis_velocity(Vector2(-HORIZONTAL_LENGTH, 0))
+		
+		if move_right:
+			set_axis_velocity(Vector2(HORIZONTAL_LENGTH, 0))
 
